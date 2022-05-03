707ecc7f519505039b5f35b256fa2e022e7cad62
mishraadityashankar <adityashankarmishra3@gmail.com>
2022-05-03 23:46:21 +0530

    First commit

---
+++ Models/ensemble_models.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from sklearn import metrics
+from sklearn import model_selection
+from sklearn.tree import DecisionTreeClassifier 
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import AdaBoostClassifier
+
+
+
+# In[1]:
+
+
+def adaBoost(X_train,y_train,X_test,y_test):
+    num_trees = 10
+    model = AdaBoostClassifier(n_estimators=num_trees, random_state=0)
+    model.fit(X_train, y_train)
+    y_pred_test = model.predict(X_test)
+    result = {
+      "Accuracy": metrics.accuracy_score(y_test, y_pred_test),
+      "F1_score": metrics.f1_score(y_test, y_pred_test, average='macro'),
+      "AUC_score": metrics.roc_auc_score(y_test,  y_pred_test),
+      "Prediction": y_pred_test
+      
+    }
+    return result
+
+
+# In[ ]:
+
+
+def bagging(X_train,y_train,X_test,y_test):
+    seed = 8
+    kfold = model_selection.KFold(n_splits = 3)
+    
+    #matrices
+    base_cls = DecisionTreeClassifier()
+  
+    # no. of base classifier
+    num_trees = 10
+
+    # bagging classifier
+    model = BaggingClassifier(base_estimator = base_cls,
+                              n_estimators = num_trees)
+  
+    model.fit(X_train, y_train)
+    y_pred_test = model.predict(X_test)
+    result = {
+      "Accuracy": metrics.accuracy_score(y_test, y_pred_test),
+      "F1_score": metrics.f1_score(y_test, y_pred_test, average='macro'),
+      "AUC_score": metrics.roc_auc_score(y_test,  y_pred_test),
+      "Prediction": y_pred_test
+      
+    }
+    return result
+
---
+++ Models/implicitModel.py
@@ -0,0 +1,48 @@
+import numpy as np
+def Implicit_ME(X_train,y_train,X_test,y_test,clustSize,experts,gate_classifier):
+    arr_X_train = np.array_split(X_train, clustSize)
+    arr_y_train = np.array_split(y_train, clustSize)
+    bestLocalExperts = []
+    pred_y_train_clust = []
+    pred_y_test_clust = []
+    X_train_new = X_train
+    X_test_new = X_test
+    count = 0
+    
+    for i in range(clustSize):
+#         clust_X_train, clust_X_test, clust_y_train, clust_y_test = train_test_split(arr_X_train[i],arr_y_train[i], test_size=0.2,random_state=1)
+        clust_X = arr_X_train[i]
+        clust_y = arr_y_train[i]
+        #index of best classifer by default 0
+        best_expert = 0
+        best_score = 0
+        
+        for j in range(len(experts)):
+            local_result = experts[j](clust_X,clust_y,clust_X,clust_y)
+            if(local_result["F1_score"] > best_score):
+                best_expert = j
+                best_score = local_result["F1_score"]
+
+        bestLocalExperts.append(best_expert)
+            
+    #taking output of train and test data from local expert 
+    for i in range(clustSize):
+        train_result = experts[bestLocalExperts[i]](arr_X_train[i],arr_y_train[i],X_train,y_train)
+        test_result =  experts[bestLocalExperts[i]](arr_X_train[i],arr_y_train[i],X_test,y_test)
+        pred_y_train_clust.append(train_result["Prediction"])
+        pred_y_test_clust.append(test_result["Prediction"])
+    
+    #adding outputs of local as features to gate
+    for data in X_train_new:
+        for i in range(clustSize):
+            np.append(data,pred_y_train_clust[i][count])
+        count = count + 1
+    count = 0
+    for data in X_test_new:
+        for i in range(clustSize):
+            np.append(data,pred_y_test_clust[i][count])
+        count = count + 1
+    
+    
+    gate_classifier(X_train_new,y_train,X_test_new,y_test)
+    
